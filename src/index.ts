2f8b7057288e22b28bdb2ef81de776b7ba5b50ea
QTN KaCa <dhnhan19999@example.com>
2021-09-23 22:03:14 +0700

    make it better

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import { config } from 'dotenv';
+config();
+
+if (process.env.NODE_ENV === 'production') {
+  require('module-alias/register');
+}
+
+import { Client, Intents } from 'discord.js';
+import { bootstrap } from './commands';
+import { scdl } from './services/soundcloud';
+import express, { Request, Response } from 'express';
+import herokuAwake from 'heroku-awake';
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_INTEGRATIONS,
+  ],
+});
+
+client.on('ready', () => {
+  console.log(`> Bot is on ready`);
+});
+
+client.login(process.env.TOKEN).then(async () => {
+  await scdl.connect();
+  bootstrap(client);
+});
+
+const app = express();
+
+app.get('/', (_req: Request, res: Response) => {
+  return res.send({
+    message: 'Bot is running',
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`> Bot is on listening`);
+  herokuAwake(process.env.APP_URL || 'http://localhost:3000');
+});
